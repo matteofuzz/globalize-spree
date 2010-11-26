347af5394cef49449615ad94bb77838316933934
matteo folin <matteo@computer-di-matteo-folin-2.local>
2010-11-26 12:34:06 +0100

    Admin edit taxon's name and description globalized as product

--- lib/globalize_spree_hooks.rb
+++ lib/globalize_spree_hooks.rb
@@ -1,4 +1,5 @@
 class GlobalizeSpreeHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
   replace :admin_product_form_left, 'admin/shared/product_globalize_form_left'
+  replace :admin_inside_taxon_form, 'admin/shared/inside_taxon_globalize_form'
 end
